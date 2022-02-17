12a439218d6d0a5a1e888267ebed3899a31a26e1
Joao-Gontijo <jvgontijo166@gmail.com>
2022-02-17 15:24:08 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Column(
+      children: [
+        Text(
+          'Delivery features faster',
+          textDirection: TextDirection.ltr,
+        ),
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        Column(
+          children: [
+            Text(
+              'Novo texto',
+              textDirection: TextDirection.ltr,
+            ),
+          ],
+        )
+      ],
+    ),
+  );
+}
