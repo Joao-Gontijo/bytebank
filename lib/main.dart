0b33f0dd138720d0c3449e2821c633b1b646d2a0
Joao-Gontijo <jvgontijo166@gmail.com>
2022-02-18 14:06:24 -0300

    refactoring into classes

--- lib/main.dart
+++ lib/main.dart
@@ -4,24 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text("100.0"),
-                subtitle: Text("9991"),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text("250.0"),
-                subtitle: Text("9991"),
-              ),
-            ),
-          ],
-        ),
+        body: ListaTransferencias(),
         appBar: AppBar(
           title: const Text('Transferências'),
         ),
@@ -32,4 +15,40 @@ void main() {
       ),
     ),
   );
+}
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ItemTransferencias(Transferencia(100, 9991)),
+        ItemTransferencias(Transferencia(350, 9991)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencias extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencias(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
 }
