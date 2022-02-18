a42791fa9323411b4b34bcdbb426dff022be8554
Joao-Gontijo <jvgontijo166@gmail.com>
2022-02-18 13:47:36 -0300

    card and listTile

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,24 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        body: Column(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text("100.0"),
+                subtitle: Text("9991"),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text("250.0"),
+                subtitle: Text("9991"),
+              ),
+            ),
+          ],
+        ),
         appBar: AppBar(
           title: const Text('Transferências'),
         ),
